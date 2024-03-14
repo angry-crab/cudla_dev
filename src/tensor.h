a3aa71713abd33e442fba4f979f5ee3c61fc23f0
Xinyu Wang <xinyu.wang@tier4.jp>
2024-03-14 16:15:31 +0900

    tensor permute

--- src/tensor.h
+++ src/tensor.h
@@ -3,10 +3,27 @@
 #include <type_traits>
 #include <cassert>
 
+namespace tensor
+{
+
+inline std::vector<int> default_stride(std::vector<int>& dim)
+{
+    std::vector<int> res(dim.size(), 1);
+    int s = 1;
+    for(int i=dim.size()-1; i>0; --i)
+    {
+        s *= dim[i];
+        res[i-1] = s;
+    }
+
+    return res;
+}
+
 template <typename T, int RANK,
             typename = std::enable_if_t<std::is_integral<T>::value || std::is_floating_point<T>::value>>
 struct Tensor
 {
+
     Tensor() = delete;
     Tensor(Tensor&& other) = delete;
     Tensor& operator=(Tensor&& other) = delete;
@@ -20,11 +37,13 @@ struct Tensor
             total *= it;
         }
         data_.reserve(total);
+        stride_ = default_stride(dim);
     }
 
     explicit Tensor(std::vector<int>& dim, std::vector<T>& data) : dim_(dim), data_(data)
     {
         assert(dim.size() == RANK);
+        stride_ = default_stride(dim);
     }
 
     explicit Tensor(const Tensor& other)
@@ -32,6 +51,7 @@ struct Tensor
         assert(other.size().size() == RANK);
         dim_ = other.size();
         data_ = other.data();
+        stride_ = other.stride();
     }
 
     inline std::vector<int> size()
@@ -44,25 +64,91 @@ struct Tensor
         return data_;
     }
 
-    void setData(std::vector<T>& data)
+    inline std::vector<int> stride()
     {
-        data_ = data;
-    }
-
-    void setDim(std::vector<int>& dim)
-    {
-        assert(dim.size() == RANK);
-        dim_ = dim;
+        return stride_;
     }
 
     Tensor& operator=(const Tensor& other)
     {
-        assert(dim.size() == RANK);
+        assert(other.size().size() == RANK);
         dim_ = other.size();
         data_ = other.data();
+        stride_ = other.stride();
+    }
+
+    void permute(const std::vector<int>& axis)
+    {
+        assert(axis.size() == dim_.size());
+        std::vector<int> new_dim(dim_.size());
+        std::vector<int> new_stride(stride_.size());
+
+        for(int i=0; i<axis.size(); ++i)
+        {
+            new_dim[i] = dim_[axis[i]];
+            new_stride[i] = stride_[axis[i]];
+        }
+        dim_ = new_dim;
+        stride_ = new_stride;
+    }
+
+    template <typename ... Ts, typename = std::enable_if_t<(std::is_convertible_v<Ts, int> && ...)>>
+    T operator()(Ts... args)
+    {
+        int index = get_index({args...});
+        return data_[index];
+    }
+
+    // void continuous()
+    // {
+    //     std::vector<T> new_data(data_.size());
+
+    // }
+
+    void pretty_print_3d()
+    {
+        for(int i=0; i<dim_[0]; ++i)
+        {
+            for(int j=0; j<dim_[1]; ++j)
+            {
+                for(int k=0; k<dim_[2]; k++)
+                {
+                    std::cout << (*this)(i,j,k) << " ";
+                }
+                std::cout << std::endl;
+
+            }
+            std::cout << std::endl;
+        }
+    }
+
+private:
+    int get_index(const std::vector<int>& coords)
+    {
+        int res = 0;
+        for(int i=0; i<stride_.size(); ++i)
+        {
+            res += stride_[i] * coords[i];
+        }
+
+        return res;
     }
 
     std::vector<int> dim_;
+    std::vector<int> stride_;
     std::vector<T> data_;
-};
 
+};
+// dim :    {2,4} -> {4,2}
+// stride : {4,1} -> {1,4}
+
+// 1 2 3 4
+// 5 6 7 8
+
+// 1 5
+// 2 6
+// 3 7
+// 4 8
+
+
+}
--- test/tensor.cc
+++ test/tensor.cc
@@ -2,13 +2,32 @@
 
 int main()
 {
-    std::vector<int> dim_a{2,3};
-    std::vector<float> a(2*3, 1.0);
-    for(int i=0;i<3;i++)
+    std::vector<int> dim_a{2,3,5};
+    std::vector<float> a(2*3*5, 1.0);
+    for(int i=0;i<2*3*5;i++)
     {
-        a[i] = 2.0;
+        a[i] = i+1;
     }
-    Tensor<float, 2> A(dim_a, a);
+    tensor::Tensor<float, 3> A(dim_a, a);
+
+
+    A.pretty_print_3d();
+
+    // A.permute({0,2,1});
+    A.permute({2,1,0});
+
+    std::cout << "permute" << std::endl;
+
+    A.pretty_print_3d();
+
+
+
+    // std::vector<int> res = tensor::default_stride(dim_a);
+    // for(auto it : res)
+    // {
+    //     std::cout << it << " ";
+    // }
+    // std::cout << std::endl;
 
     return 0;
 }
